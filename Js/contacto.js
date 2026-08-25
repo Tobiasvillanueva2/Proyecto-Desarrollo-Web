70d3f5dfb64144202c4e37da5b2998822a8d6baf
TobiasVillanueva2 <tvillanuevacordoba@gmail.com>
2026-08-25 01:02:14 -0300

    modificaciones finales al js ,html y css

--- Js/contacto.js
+++ Js/contacto.js
@@ -1,6 +1,5 @@
-// 1. REEMPLAZA CON TU PUBLIC KEY
-
-emailjs.init("_yFz1xFE0WGUKFjY8");
+// 1. REEMPLAZA CON TU PUBLIC KE
+emailjs.init("_7g1UfUabbNC7uf1i");
 console.log("EmailJS initialized with public key.");
 const form = document.getElementById("form-consultas");
 const btn = document.getElementById("btn-submit");
@@ -13,7 +12,7 @@ form.addEventListener("submit", function (e) {
 
   // 2. REEMPLAZA CON TU SERVICE_ID Y TEMPLATE_ID
   emailjs
-    .sendForm("service_xgsdd8l", "template_bntnune", this)
+    .sendForm("service_8klt4lp", "template_y9ydf3s", this)
     .then(() => {
       alert("¡Correo enviado con éxito!");
       form.reset();
@@ -27,3 +26,35 @@ form.addEventListener("submit", function (e) {
       btn.textContent = "Enviar";
     });
 });
+
+
+/* no podemos poner un segundo formulario. causa desconocida
+
+emailjs.init("_7g1UfUabbNC7uf1i");
+console.log("EmailJS initialized with public key.");
+const form2 = document.getElementById("form-trabajo");
+const btn2 = document.getElementById("btn-trabajo");
+
+form2.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  btn2.disabled = true;
+  btn2.textContent = "Enviando...";
+
+  // 2. REEMPLAZA CON TU SERVICE_ID Y TEMPLATE_ID
+  emailjs
+    .sendForm("service_8klt4lp", "template_ex0keao", this)
+    .then(() => {
+      alert("¡Correo enviado con éxito!");
+      form2.reset();
+    })
+    .catch((err) => {
+      alert("Error al enviar el correo. Por favor intenta de nuevo.");
+      console.error("EmailJS Error:", err);
+    })
+    .finally(() => {
+      btn2.disabled = false;
+      btn2.textContent = "Enviar";
+    });
+});
+*/
